45570c75165fc0151c5b0993e2f99febfd66b034
Susan Theis <sttheis@hotmail.com>
2021-01-11 17:38:49 -0600

    Small refactor

--- timelog.py
+++ timelog.py
@@ -2,6 +2,19 @@ import distutils.util
 import time
 from datetime import date
 
+
+def save_as_txt(project_name, hours, minutes, quality):
+    today = date.today().strftime("%B %d, %Y")
+    with open('work_log.txt', 'a') as f:
+        f.write(today + '\n')
+        f.write(f'Worked on: {project_name}\n')
+        if hours == 0:
+            f.write(f'Time spent: {minutes} minutes\n')
+        else:
+            f.write(f'Time spent: {hours} hours, {minutes} minutes\n')
+        f.write(f'Quality rating: {quality} out of 5\n\n')
+
+
 if __name__ == '__main__':
     keep_working = True
     while keep_working:
@@ -17,15 +30,7 @@ if __name__ == '__main__':
 
         print(f'You worked for {hours} hours and {minutes} minutes')
         quality = input('How focused were you on the work? (rate out of 5): ')
-        today = date.today().strftime("%B %d, %Y")
 
-        with open('work_log.txt', 'a') as f:
-            f.write(today + '\n')
-            f.write(f'Worked on: {project_name}\n')
-            if hours == 0:
-                f.write(f'Time spent: {minutes} minutes\n')
-            else:
-                f.write(f'Time spent: {hours} hours, {minutes} minutes\n')
-            f.write(f'Quality rating: {quality} out of 5\n\n')
+        save_as_txt(project_name, hours, minutes, quality)
 
         keep_working = distutils.util.strtobool(input('Would you like to work on something else now? '))
